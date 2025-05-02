05d217b26853b46a395be6fba05009003c09ff7f
rishav76dev <rjrishav037@gmail.com>
2025-05-02 16:27:12 +0530

    middleware

---
+++ backend/middleware/authMIddleware.js
@@ -0,0 +1,25 @@
+// middleware/authMiddleware.js
+import jwt from 'jsonwebtoken';
+const JWT_SECRET = process.env.JWT_SECRET || 'yoursecretkey';
+
+export const authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: 'No token provided' });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; // { userId, role }
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+export const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access forbidden: insufficient role' });
+    }
+    next();
+  };
+};
